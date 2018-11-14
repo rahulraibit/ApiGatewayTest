d4c322fabbd805cf09fdc60cf293f7158060a440
shubhi.gupta@honeywell.com <shubhi.gupta@honeywell.com>
2018-11-14 17:16:59 +0530

    Created new dummy application

---
+++ ApplicationGatewayTest/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ApplicationGatewayTest.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ApplicationGatewayTest/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ApplicationGatewayTest.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
